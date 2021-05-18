f7e06366da42c8815e96fb32c8dca4d0b0ee47ef
Chris Franklin <cef1911@hotmail.com>
2021-05-18 15:18:46 -0400

    Add files via upload
    resubmitted Budget assignment

---
+++ classes_lmsassignment.py
@@ -0,0 +1,73 @@
+# class Budget:
+#     pass
+
+# obj = Budget()
+# print(type(obj))
+
+# class Budget:
+    
+#     amount = 0
+
+#     def_init_(self):
+#         self.category = ["Food", "Clothing", "Entertainment"]
+
+
+class Category:
+
+    #constructor
+    def __init__ (self, category, amount):
+        self.category = category
+        self.amount = amount
+        
+
+    #methods
+    def deposit(self, amount):
+        # pass
+        #return 'this is a deposit method'
+        self.amount += amount
+        return "You have successfully deposited {} in the {} category".format(amount, self.category)
+
+    def budget_balance(self):
+        return "This is the current balance: {}".format(self.amount)
+    
+    def check_balance(self):
+        #pass
+        return "This is the check balance: {}".format(self.amount)
+
+    def withdraw(self, amount):
+        #pass
+        self.amount -= amount
+        return "You have successfully withdrew {} in the {} category".format(amount, self.category)
+
+    def transfer(self, amount, category):
+        category.amount -= amount
+        self.amount += amount
+        return "You have successfully transferred {} in {} category to {} category".format(amount, category.category, self.category)
+       
+
+    
+    
+
+food_category = Category ("Food", 500)    
+clothing_category = Category ("Clothing", 300)    
+car_category = Category ("Car Expenses", 600)    
+
+print(food_category.deposit(250))
+print(food_category.budget_balance())
+#print(category.deposit())
+
+print(clothing_category.deposit(300))
+print(clothing_category.check_balance())
+
+print(car_category.withdraw(400))
+print(car_category.budget_balance())
+
+
+print(car_category.transfer(200, food_category))
+
+
+   
+
+
+
+
